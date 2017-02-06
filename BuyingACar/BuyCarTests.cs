a9ea4185788ea07e59ea0bdcf8eceb9af035e1c6
hatelove <jay.hatelove@gmail.com>
2017-02-06 15:03:27 +0800

    initialized and add first two test cases

---
+++ BuyingACar/BuyCarTests.cs
@@ -0,0 +1,32 @@
+﻿using BuyingACar;
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class BuyCarTests
+{
+    [Test]
+    public void nbMonths_old_2000_new_8000_perMonthSave_1000_percentLoss_1point5()
+    {
+        int[] r = new int[] { 0, 4000 };
+        Assert.AreEqual(r, BuyCar.nbMonths(12000, 8000, 1000, 1.5f));
+    }
+
+    [Test]
+    public void nbMonths_old_12000_new_8000_perMonthSave_1000_percentLoss_1point5()
+    {
+        int[] r = new int[] { 6, 766 };
+        Assert.AreEqual(r, BuyCar.nbMonths(2000, 8000, 1000, 1.5f));
+    }
+}
+
+namespace BuyingACar
+{
+    internal class BuyCar
+    {
+        public static int[] nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, float percentLossByMonth)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
